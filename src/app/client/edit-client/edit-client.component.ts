3b41b1728ea554c0665c536d11ec0d74d4e80c9b
Vanessa Burkot <vanessaburkot1@gmail.com>
2021-10-21 20:54:37 -0300

    Editar Client

--- src/app/client/edit-client/edit-client.component.ts
+++ src/app/client/edit-client/edit-client.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Client } from 'src/app/shared';
+import { ClientService } from '../services';
 
 @Component({
   selector: 'app-edit-client',
@@ -6,10 +10,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-client.component.css']
 })
 export class EditClientComponent implements OnInit {
+  @ViewChild('formClient') formClient!: NgForm;
+  client!: Client;
 
-  constructor() { }
+  constructor(
+    private clientService: ClientService,
+    private route: ActivatedRoute,
+    private router: Router
+
+  ) { }
 
   ngOnInit(): void {
+    this.getClient(this.route.snapshot.params.id);
   }
 
+  private getClient(id: number) {
+    this.clientService.getClient(id).subscribe(
+      (client: Client) => {
+        this.client = client;
+      },
+      (error) => {
+        alert(error.error.message);
+      }
+    );
+  }
+
+  private updateClient(client: Client){
+    this.clientService.updateClient(client).subscribe(
+      (client: Client) => {
+        this.client = client;
+      },
+      (error) => {
+        alert(error.error.message);
+      }
+    );
+  }
+
+  public onSubmit() {
+    if (this.formClient.valid) {
+      this.updateClient(this.client);
+      this.router.navigate(['/clients']);
+    }
+  }
 }
